bcc2d7dba0e514c6fcdc20dfbced061526b42562
Katarzyna Dylewska <kad54@cam.ac.uk>
2022-02-01 17:21:28 +0000

    Update geo.py
    Added stations_by_river and rivers_by_station_number from my fork

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -27,18 +27,66 @@ def stations_by_distance(stations, p):
     #sort list by distance
     stationList = sorted_by_key(stationList, 2)
 
-    return stationList
+def rivers_with_station(stations):
+    """Given a list of station objects, returns a container list with the names of the rivers 
+        with a monitoring station"""
 
-def stations_within_radius(stations, centre, r):
-    """Return a list of all stations within radius r of a geographic
-    coordinate centre"""
-    qualifyingStations = []
+    # Create an empty list to hold the rivers
+    rivers = []
 
-    # build list of stations within required radius
+    # Iterate through the stations and append the river into the list if it is not already in the list
     for station in stations:
-        coords = station.coord
-        distance = haversine(coords, centre) # use haversine to calculate distance in km
-        if distance < abs(r):
-            qualifyingStations.append(station.name)
+        if station.river not in rivers:
+            rivers.append(station.river)
     
-    return qualifyingStations
+    # Sort by alphabetical order
+    return sorted(rivers)
+
+def stations_by_river(stations):
+
+    # Create an empty dict to hold the rivers
+    rivers_and_stations = {}
+
+    # Iterate through all stations
+    for station in stations:
+
+        # Add the river and station name to the dict if the river is not already in the dict
+        if station.river not in rivers_and_stations.keys():
+            rivers_and_stations[station.river]=[station.name]
+
+        # Append the station name to the corresponding river (key) in the dict
+        else:
+            rivers_and_stations[station.river].append(station.name)
+
+    # Iterate through all dict items and sort the lists of station names (value) in alphabetical order
+    for key in rivers_and_stations:
+        rivers_and_stations[key].sort()
+
+    return rivers_and_stations
+
+def rivers_by_station_number(stations, N):
+
+    # Create an empty dict to hold the rivers
+    rivers = {}
+
+    # Iterate through all stations
+    for station in stations:
+        # Add the river to the dict and initialize station count if the river is not already in the dict
+        if station.river not in rivers.keys():
+            rivers[station.river] = 1
+        else:
+            rivers[station.river] += 1
+
+    rivers_sorted = sorted_by_key([(key, rivers[key]) for key in rivers], 1, reverse=True)
+
+    if N > len(rivers_sorted):
+        end_index = len(rivers_sorted)
+    else:
+        for i in reversed(range(len(rivers_sorted))):
+            if rivers_sorted[i][1] == rivers_sorted[N-1][1]:
+                end_index = i
+                break
+
+    return rivers_sorted[:end_index+1]
+
+    return stationList
