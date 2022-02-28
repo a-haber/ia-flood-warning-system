7bccc326220b83d26fc0ab1962f4c039171ef5a5
Katarzyna Dylewska <kad54@cam.ac.uk>
2022-02-28 22:01:34 +0000

    Update flood.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -7,11 +7,33 @@ def stations_level_over_threshold(stations, tol):
         water level is above tol and the relative water level at that station"""
 
     flooded_stations = []
+
     stationdata.update_water_levels(stations)
+
     for station in stations:
         if station.relative_water_level(station.latest_level) is not None:
             if station.relative_water_level(station.latest_level) > tol:
                 station_name_water_level = station.name, station.relative_water_level(station.latest_level)
                 flooded_stations.append(station_name_water_level)
+
     sorted_flooded_stations = sorted_by_key(flooded_stations, 1, reverse=True)
+
     return sorted_flooded_stations
+
+def stations_highest_rel_level(stations, N):
+
+    """Returns a list of the N stations at which the water level,
+    relative to the typical range, is highest"""
+
+    stations_relative_level = []
+
+    stationdata.update_water_levels(stations)
+
+    for station in stations:
+        if station.relative_water_level(station.latest_level) is not None:
+            station_name_water_level = station.name, station.relative_water_level(station.latest_level)
+            stations_relative_level.append(station_name_water_level)
+
+    sorted_stations_relative_level = sorted_by_key(stations_relative_level, 1, reverse=True)
+
+    return sorted_stations_relative_level[:N]
