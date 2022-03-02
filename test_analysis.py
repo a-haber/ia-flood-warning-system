9707b3a6d66bd5be38e7daa205ce8e60c64d9841
Aran Haber <ah2204@cam.ac.uk>
2022-03-02 00:41:29 +0000

    Tweaking test

--- test_analysis.py
+++ test_analysis.py
@@ -5,17 +5,18 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.analysis import polyfit
 import numpy
+import matplotlib
 
 def test_polyfit():
     # Build list of dates and levels
     stationlist = build_station_list()
-    for station in stationlist[:20]:
+    for station in stationlist[:5]:
         measure_id = station.measure_id
         dates, levels = fetch_measure_levels(measure_id, datetime.timedelta(days=5))
 
-    # Check that the function outputs the correct type of data
-    output = polyfit(dates, levels, 4)
-    assert isinstance(output, tuple)
-    assert isinstance(output[0], numpy.poly1d)
-    assert isinstance(output[1], float)
-    assert len(output) == 2
+        # Check that the function outputs the correct type of data
+        output = polyfit(dates, levels, 4)
+        assert isinstance(output, tuple)
+        assert isinstance(output[0], numpy.poly1d)
+        assert isinstance(output[1], float)
+        assert len(output) == 2
