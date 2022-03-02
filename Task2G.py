4875f77e8ba25e9bd9d93a8b191e8c6e16ae2885
Aran Haber <ah2204@cam.ac.uk>
2022-03-02 21:46:46 +0000

    upgrade warning on some high risk stations

--- Task2G.py
+++ Task2G.py
@@ -1,6 +1,11 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.analysis import polyfit
+import numpy as np
+import datetime
+import matplotlib.dates
 
-def main():
+def run():
     severe = []
     high = []
     moderate = []
@@ -23,9 +28,9 @@ def main():
             # >150%  - severe risk
             # 120% - 150%  - high risk
             # 90% - 120% - moderate risk
-            # >90% - low risk
+            # <90% - low risk
 
-            percentage = (station.latest_level - station.typical_range[0]) / (station.typical_range[1] - station.typical_range[0])
+            percentage = station.relative_water_level(station.latest_level)
 
             if percentage >= 1.5:
                 severe.append(station.name)
@@ -37,7 +42,29 @@ def main():
                 low.append(station.name)
         else:
             no_data.append(station.name)
-
+        
+        # For stations identified as high risk, estimate if water level
+        # is rising or falling using the polyfit best fit function
+        # Then, if water level rising, upgrade warning level to severe
+        if station.name in high:
+            try:
+                # Create best-fit polynomial to approximate water level data over past 5 days
+                measure_id = station.measure_id
+                dates, levels = fetch_measure_levels(measure_id, datetime.timedelta(days=5))
+                bestfit, offset = polyfit(dates, levels, p=4)
+                derivative = np.polyder(bestfit) # Differentiate best-fit polynomial
+                # Estimate if water level rising or falling at the most recent data point
+                gradient = derivative(matplotlib.dates.date2num(dates[0]) - offset) # Evaluate gradient
+                # Gradients which are positive but close to 0 are ignored here
+                if gradient > 1:
+                    high.remove(station.name)
+                    severe.append(station.name)
+            # Some stations have faulty data - no useful polynomial fit, so leave in current warning category
+            except IndexError:
+                pass
+            except KeyError:
+                pass
+    
     severe.sort()
     high.sort()
     moderate.sort()
@@ -46,8 +73,8 @@ def main():
     print(f"Severe Risk:\n{severe}\n")
     print(f"High Risk:\n{high}\n")
     print(f"Moderate Risk:\n{moderate}\n")
-    print(f"Low Risk:\n{low}\n")
-    print(f"No Reliable Data:\n{no_data}\n")
+    print(f"Low Risk:\n{len(low)} stations\n")
+    print(f"No Reliable Data:\n{len(no_data)} stations\n")
 
 if __name__ == "__main__":
-    main()
+    run()
