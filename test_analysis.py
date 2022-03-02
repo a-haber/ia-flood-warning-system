21d6cfdb3225c2e1c14949f7ede08b07cfd5543b
Aran Haber <ah2204@cam.ac.uk>
2022-03-02 00:33:16 +0000

    Add test for 2F + run deliverables

---
+++ test_analysis.py
@@ -0,0 +1,21 @@
+"""Test for analysis module"""
+
+import datetime
+from floodsystem.stationdata import build_station_list
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.analysis import polyfit
+import numpy
+
+def test_polyfit():
+    # Build list of dates and levels
+    stationlist = build_station_list()
+    for station in stationlist[:20]:
+        measure_id = station.measure_id
+        dates, levels = fetch_measure_levels(measure_id, datetime.timedelta(days=5))
+
+    # Check that the function outputs the correct type of data
+    output = polyfit(dates, levels, 4)
+    assert isinstance(output, tuple)
+    assert isinstance(output[0], numpy.poly1d)
+    assert isinstance(output[1], float)
+    assert len(output) == 2
